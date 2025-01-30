2967d825744ac7f962eaf8e76b93995a0e3bca97
Haruto Ikehara <kduxwr@gmail.com>
2025-01-30 18:50:28 +0900

    Fix functions bugs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,11 +4,12 @@ import { getFirestore } from "firebase-admin/firestore"
 import { ServiceAccount } from "firebase-admin/lib/app/credential"
 import { getStorage } from "firebase-admin/storage"
 import * as functions from "firebase-functions"
-import serviceAccount from "../keys/serviceAccountKey.json"
+import serviceAccount from "../keys/serviceAccountKey.json" with { type: "json" }
 import { GetDownloadurlParams } from "./types"
 
 initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
+  storageBucket: "hi-storage.firebasestorage.app",
 })
 
 functions.setGlobalOptions({
@@ -17,43 +18,49 @@ functions.setGlobalOptions({
   timeoutSeconds: 60,
 })
 
-export const getDownloadUrl = functions.https.onRequest(async (req, res) => {
-  try {
-    const { fileId, password } = req.body as GetDownloadurlParams
+export const getDownloadUrl = functions.https.onRequest(
+  { cors: ["http://localhost:8080", "https://hi-storage.web.app"] },
+  async (req, res) => {
+    try {
+      const { fileId, password } = req.body as GetDownloadurlParams
 
-    const db = getFirestore()
-    const fileRef = db.collection("files").doc(fileId)
-    const fileData = await fileRef.get()
+      const db = getFirestore()
+      const fileRef = db.collection("files").doc(fileId)
+      const fileData = await fileRef.get()
 
-    if (!fileData.exists) {
-      res.status(404).json({ error: "File does not exist" })
+      if (!fileData.exists) {
+        res.status(404).json({ error: "File does not exist" })
+        return
+      }
+
+      const storedPasswordHash = fileData.data()?.password
+
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        storedPasswordHash,
+      )
+
+      if (!isPasswordCorrect) {
+        res.status(401).json({ error: "Incorrect password" })
+        return
+      }
+
+      const bucket = getStorage().bucket()
+      const file = bucket.file(fileData.data()?.storagePath)
+      const [url] = await file.getSignedUrl({
+        action: "read",
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 1週間
+      })
+
+      res.status(200).json({ url })
+
       return
+    } catch (error) {
+      functions.logger.error("Error in getDownloadUrl function:", error)
+      res.status(500).json({ error: "Internal server error" })
     }
-
-    const storedPasswordHash = fileData.data()?.password
-
-    const isPasswordCorrect = await bcrypt.compare(password, storedPasswordHash)
-
-    if (!isPasswordCorrect) {
-      res.status(401).json({ error: "Incorrect password" })
-      return
-    }
-
-    const bucket = getStorage().bucket()
-    const file = bucket.file(fileData.data()?.storagePath)
-    const [url] = await file.getSignedUrl({
-      action: "read",
-      expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 1週間
-    })
-
-    res.status(200).json({ url })
-
-    return
-  } catch (error) {
-    functions.logger.error("Error in getDownloadUrl function:", error)
-    res.status(500).json({ error: "Internal server error" })
-  }
-})
+  },
+)
 
 export const generateHashedPassword = functions.https.onRequest(
   async (req, res) => {
